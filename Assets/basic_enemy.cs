ae51ed0b3e656886f8bd2ae8a16649d99740ab3f
bigmindsdev <bigmindsdev@gmail.com>
2020-11-05 12:51:02 -0800

    initial commit

---
+++ Assets/basic_enemy.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum StartDirection
+{
+    LEFT,
+    RIGHT,
+}
+
+public class basic_enemy : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private BoxCollider2D boxCollider2d;
+    private SpriteRenderer sprite;
+    [SerializeField]
+    private StartDirection startDirection = StartDirection.LEFT;
+    [SerializeField]
+    private int maxDist = 2;
+    [SerializeField]
+    private int moveSpeed = 1;
+    private float startLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        boxCollider2d = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        startLocation = transform.position.x;
+        if (startDirection == StartDirection.LEFT)
+        {
+            rb2d.velocity = new Vector2(-moveSpeed, 0);
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(moveSpeed, 0);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.position.x < startLocation - maxDist)
+        {
+            rb2d.velocity = new Vector2(-rb2d.velocity.x, 0);
+        }
+        else if (transform.position.x > startLocation + maxDist)
+        {
+            rb2d.velocity = new Vector2(-rb2d.velocity.x, 0);
+        }
+        else
+        {
+            rb2d.velocity = rb2d.velocity;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum JumpState
+{
+    FALLING,
+    JUMPING,
+    GROUNDED
+}
+
+public class movement : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private BoxCollider2D boxCollider2d;
+    private SpriteRenderer sprite;
+    private float hInput = 0;
+    private float maxAirSpeed = 2f;
+    private float maxFallSpeed = -10f;
+    private JumpState jumpState = JumpState.GROUNDED;
+    private RaycastHit2D groundcastHit;
+    [SerializeField]
+    private LayerMask groundLayerMask;
+    [SerializeField]
+    private int movesspeed = 1;
+    [SerializeField]
+    private int jumpForce = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        boxCollider2d = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log(jumpState);
+        hInput = Input.GetAxis("Horizontal");
+
+        groundcastHit = DoBoxCast(0.1f);
+        if (rb2d.velocity.y < 0.01 && rb2d.velocity.y > -0.01)
+        {
+            jumpState = JumpState.GROUNDED;
+        }
+
+        if (Input.GetKeyDown("space") && jumpState == JumpState.GROUNDED)
+        {
+            Debug.Log("JUMPING");
+            rb2d.velocity = new Vector2(rb2d.velocity.x, 0);                  //zero out y velocity so new force is fresh / consistent
+            rb2d.AddForce(new Vector2(0, 150*jumpForce));
+            jumpState = JumpState.JUMPING;
+        }
+        if (rb2d.velocity.y < 0)
+        {
+            jumpState = JumpState.FALLING;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (hInput != 0)
+        {
+            HandleHorizontalMovement();
+        }
+        //Limit fall speed if necesary
+        rb2d.velocity = (new Vector2(rb2d.velocity.x, Mathf.Clamp(rb2d.velocity.y, maxFallSpeed, 50)));
+    }
+
+    private void HandleHorizontalMovement()
+    {
+        rb2d.velocity = new Vector2(hInput*movesspeed, rb2d.velocity.y);
+    }
+
+    private RaycastHit2D DoBoxCast(float distanceBelowPlayer)
+    {
+        RaycastHit2D result;
+        //Boxcast slightly below the player, looking for objects on the groundLayerMask
+        result = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, distanceBelowPlayer, groundLayerMask);
+
+        //Set debug ray colors based on grounded or not
+        Color rayColor;
+        if (groundcastHit.collider != null)
+        {
+            rayColor = Color.green;
+        }
+        else
+        {
+            rayColor = Color.red;
+        }
+        //Draw rays to visualize the box cast
+        Debug.DrawRay(boxCollider2d.bounds.center + new Vector3(boxCollider2d.bounds.extents.x, 0), Vector2.down * (boxCollider2d.bounds.extents.y + distanceBelowPlayer), rayColor);
+        Debug.DrawRay(boxCollider2d.bounds.center - new Vector3(boxCollider2d.bounds.extents.x, 0), Vector2.down * (boxCollider2d.bounds.extents.y + distanceBelowPlayer), rayColor);
+        Debug.DrawRay(boxCollider2d.bounds.center - new Vector3(boxCollider2d.bounds.extents.x, boxCollider2d.bounds.extents.y + distanceBelowPlayer), Vector2.right * (boxCollider2d.bounds.extents.x * 2), rayColor);
+
+        //Return whether or not the boxcast hit ground
+        return result;
+    }
+}
